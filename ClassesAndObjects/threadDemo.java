f2d1e70782d89b5be812183ed40d0acd629e0a58
Parth Salunke <144267660+sparth292@users.noreply.github.com>
2025-03-11 01:10:15 +0530

    Add files via upload

--- ClassesAndObjects/threadDemo.java
+++ ClassesAndObjects/threadDemo.java
@@ -1,32 +1,43 @@
-class AscendingOrder implements Runnable{
-public void run(){
-     for(int i = 1 ; i <= 10 ; i++){
-        System.out.println(i);
-     }
-}
-}
-
-class DescendingOrder implements Runnable{
-public void run(){
-     for(int i = 10 ; i > 0 ; i--){
-        System.out.println(i);
-     }
-}
+class Red extends Thread {
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Red Light - " + i);
+        }
+    }
 }
 
-class threadDemo{
-
-public static void main(String args[]){
-       AscendingOrder asc1 = new AscendingOrder();
-       DescendingOrder desc1 = new DescendingOrder();
-       Thread t1 = new Thread(asc1);
-       Thread t2 = new Thread(desc1);
-       t2.setPriority(10);
-       t1.setPriority(1);
-       System.out.println("asc1 priority : " + t1.getPriority());
-       System.out.println("desc1 priority : " + t2.getPriority());
-       t2.start();
-       t1.start();                     
+class Yellow extends Thread {
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Yellow Light - " + i);
+        }
+    }
 }
 
+class Green extends Thread {
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Green Light - " + i);
+        }
+    }
+}
+
+public class threadDemo {
+    public static void main(String[] args) {
+        Red red = new Red();
+        Yellow yellow = new Yellow();
+        Green green = new Green();
+
+        red.setPriority(10);
+        yellow.setPriority(5);
+        green.setPriority(1);
+
+        System.out.println("Red light priority: " + red.getPriority());
+        System.out.println("Yellow light priority: " + yellow.getPriority());
+        System.out.println("Green light priority: " + green.getPriority());
+
+        red.start();
+        yellow.start();
+        green.start();
+    }
 }
